ce9a97cb5b0cc787082dc64b98f9ff6268784583
maheshbhosale7648 <maheshbhosale7648@gmail.com>
2024-03-11 14:32:05 +0000

    today commit

--- myjava.java
+++ myjava.java
@@ -1,3 +1,9 @@
-public static void main(string[] args) {
-	System.out.println("Hello, Students!");
+class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!"); 
+
+    }
+
 }
